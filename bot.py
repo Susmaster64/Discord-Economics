d25a3fb8361d5b64a1093114a940a22e968a4782
Susmaster64 <99788724+Susmaster64@users.noreply.github.com>
2023-10-22 07:02:39 +0000

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,64 @@
+import hikari
+
+intents = hikari.Intents(hikari.Intents.ALL)
+bot = hikari.GatewayBot(token='MTEwNzUzODEyMTkzNzEyNTQwNg.GpymMa.gtZQsHzrsiDRqCt3XXz0HwDmDJF_rVnvLm70lE', intents=intents)
+    
+user_id_list = [580852295273545749, 767875224363728926, 487730092617760789, 905530626399158274, 590023274923163648, 936449954329792602, 567962682154680322, 596551827164495887, 755644125935566851, 761019409937596416, 696923816760180787, 634895849159262239, 516095493843976194, 741163038870208512, 578083125272379407, 880295376945577994]
+
+@bot.listen(hikari.DMMessageCreateEvent)
+async def analCumSex(event):    
+    
+    if event.author_id in user_id_list:
+        with open('balances.txt') as f:
+            balances = list(f.read().split(','))
+            
+        contents = event.content.lower().strip()
+        if contents == 'bal' or contents == 'balance':
+            
+            for n in range(len(user_id_list)):
+                if user_id_list[n] == event.author_id:
+                    output = balances[n]
+                    break
+
+            await event.author.send(f'Your current account balance is {output} :eggplant:s')
+        
+        elif list(event.content.lower().split(' '))[0] == 'send':
+            try:
+                transfer_amount = float(list(event.content.lower().split(' '))[1])
+                for n in range(len(user_id_list)):
+                    if user_id_list[n] == event.author_id:
+                        output = balances[n]
+                        index = n
+                        break
+                        print('y')
+                
+                if float(output) < transfer_amount:
+                    await event.author.send('It\'s haram to be in debt')
+                    print('y')
+                    
+                else:
+                    if int(list(event.content.lower().split(' '))[2]) in user_id_list and int(list(event.content.lower().split(' '))[2]) != event.author_id:
+                        print(list(event.content.lower().split(' '))[2])
+                        print(list(event.content.lower().split(' '))[2])
+                        new_bal = float(output) - float(list(event.content.lower().split(' '))[1])
+                        user = await bot.rest.fetch_user(list(event.content.lower().split(" "))[2])
+                        await event.author.send(f'Sending {transfer_amount} to {user}. \nYou currently have {output} :eggplant:s in your account. Making this transaction brings you down to {new_bal}')       
+                        
+                        balances[index] = str(new_bal)
+                        with open('balances.txt', 'w') as f:
+                            f.write(','.join(balances))         
+                    else:
+                        print(list(event.content.lower().split(' '))[2])
+                        print(list(event.content.lower().split(' '))[2])
+                        await event.author.send(f'You cannot send :eggplant:s to that user ID.')
+                
+                
+            
+            except Exception as exc:
+                print(exc)
+                await event.author.send('Amount to send *must* be a valid floating point number.')
+        
+        else:
+            await event.author.send('Use `bal` or `balance` to check your balance. Use `send <send amount> <recipient\'s user ID>` to send someone some :eggplant:s')
+        
+bot.run()
